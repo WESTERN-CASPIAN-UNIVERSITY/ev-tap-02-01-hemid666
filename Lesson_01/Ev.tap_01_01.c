5ba6cd597675657aa5f5765c36b9a503e2400fec
hemid666 <84330351+hemid666@users.noreply.github.com>
2026-03-03 09:54:37 +0400

    Update Ev.tap_01_01.c(hemid)

--- Lesson_01/Ev.tap_01_01.c
+++ Lesson_01/Ev.tap_01_01.c
@@ -13,4 +13,31 @@ Yaş: 20
 Universitet: Bakı Dövlət Universiteti
 İxtisas: Kompüter Elmləri*/
 
+#include <stdio.h>
+int main (){
+	char ad[50], soyad[50], universitet[100], ixtisas [100];
+	int yas;
+	printf("adinzi daxil edin:");
+	scanf("%s",ad);
+	
+	printf("soyadinizi daxil edin:");
+	scanf("%s", soyad);
+	
+	printf("yasinizi daxil edin:");
+	scanf("%d", &yas);
+	
+	printf("universitetinizi daxil edin:");
+	scanf("%[\n]", universitet);
+	
+	printf("ixtisasinizi daxil edin:");
+	scanf("%[^\n]",ixtisas);
+	
+	printf("\n--- Melumatlar ---\n");
+	printf("Ad: %s\n", ad);
+	printf("Soyad: %s\n", soyad);
+	printf("Yas: %d\n", yas);
+	printf("Universitet: %s\n", universitet);
+	printf("Ixtisas: %s\n", ixtisas);
 
+	return 0;
+}	
